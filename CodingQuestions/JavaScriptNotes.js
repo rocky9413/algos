b09497d1c2736ce6ebdf835db0945e081de2ba07
Rocky Lin <liangwen511@gmail.com>
2022-04-18 21:48:31 -0400

    JS-notes

---
+++ CodingQuestions/JavaScriptNotes.js
@@ -0,0 +1,17 @@
+/**
+ * JavaScript General Notes
+ */
+
+/**
+ * UTF-8 ASCII character set
+ * method: charCodeAt()
+ * A-Z: 65 - 90
+ * a-z: 97 - 122
+ */
+
+/**
+ * JavaScript Number Reference
+ * isNaN() | toString() | parseInt()
+ * Number.POSITIVE_INFINITY
+ * Number.NEGATIVE_INFINITY
+ */
