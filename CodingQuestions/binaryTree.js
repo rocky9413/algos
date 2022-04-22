1baf846c99d94045871c23514b51f2c915d77fc5
Rocky Lin <liangwen511@gmail.com>
2022-04-22 18:15:43 -0400

    bst-dfs-bfs

--- CodingQuestions/binaryTree.js
+++ CodingQuestions/binaryTree.js
@@ -33,7 +33,6 @@ const depthFirstSearchRecursive = (root) => {
   const right = depthFirstSearchRecursive(root.right);
   return [root.val, ...left, ...right];
 }
-
 const breathFirstSearchByQueue = (root) => {
   if (!root) return [];
   const result = [];
@@ -47,6 +46,107 @@ const breathFirstSearchByQueue = (root) => {
   return result;
 }
 
+const includesBFS = (root, target) => {
+  if (!root) return false;
+  const queue = [root];
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    if (curr.val === target) return true;
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+  return false;
+}
+const includesDFSRecursive = (root, target) => {
+  if (!root) return false;
+  if (root.val === target) return true;
+  const left = includesDFSRecursive(root.left, target);
+  const right = includesDFSRecursive(root.right, target);
+  return left || right;
+}
+
+const sumDFSStack = (root) => {
+  if (!root) return 0;
+  let sum = 0;
+  const stack = [root];
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    sum += curr.val;
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
+  }
+  return sum;
+}
+const sumDFSRecursive = (root) => {
+  if (!root) return 0;
+  const left = sumDFSRecursive(root.left);
+  const right = sumDFSRecursive(root.right);
+  return root.val + left + right;
+}
+const sumBFSQueue = (root) => {
+  if (!root) return 0;
+  const queue = [root];
+  let sum = 0;
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    sum += curr.val;
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+  return sum;
+}
+
+const minValueDFS = (root) => {
+  if (!root) return -1;
+  const stack = [root];
+  let min = Number.POSITIVE_INFINITY;
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    min = Math.min(min, curr.val);
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
+  }
+  return min;
+}
+const minValueDFSRecursive = (root) => {
+  if (!root) return Number.POSITIVE_INFINITY;
+  const left = minValueDFSRecursive(root.left);
+  const right = minValueDFSRecursive(root.right);
+  return Math.min(root.val, left, right);
+}
+const minValueBFS = (root) => {
+  if (!root) return -1;
+  const queue = [root];
+  let min = Number.POSITIVE_INFINITY;
+  while (queue.length > 0) {
+    const curr = queue.shift();
+    min = Math.min(min, curr.val);
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+  return min;
+}
+
+const maxSumPathDFS = (root) => {
+  // if (!root) return 0;
+  // const stack = [];
+  // let maxSum = 0;
+  // let currSum = 0;
+  // while (stack.length > 0) {
+  //   const curr = stack.pop();
+  //   maxSum += curr.val;
+  //   if (!curr.left && curr.right) maxSum += curr.right.val;
+  //   if (!curr.right && curr.left) maxSum += curr.left.val;
+  //   if (curr.left && curr.right) maxSum += Math.max(curr.right.val, curr.right.val)
+
+  // }
+  if (!root) return Number.NEGATIVE_INFINITY;
+  if (!root.left && !root.right) return root.val;
+  const left = maxSumPathDFS(root.left);
+  const right = maxSumPathDFS(root.right);
+  return Math.max(left, right) + root.val;
+}
+
 // find minimum depth
 const minDepth = function (root) {
   if (!root) return 0;
@@ -54,6 +154,7 @@ const minDepth = function (root) {
   if (!root.right) return minDepth(root.left) + 1;
   return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
+
 // find maximum depth
 const maxDepth = function (root) {
   return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0;
@@ -72,4 +173,13 @@ module.exports = {
   depthFirstSearchByStack,
   depthFirstSearchRecursive,
   breathFirstSearchByQueue,
+  includesBFS,
+  includesDFSRecursive,
+  sumDFSStack,
+  sumDFSRecursive,
+  sumBFSQueue,
+  minValueDFS,
+  minValueDFSRecursive,
+  minValueBFS,
+  maxSumPathDFS,
 };
--- CodingQuestions/binaryTree.test.js
+++ CodingQuestions/binaryTree.test.js
@@ -2,6 +2,15 @@ const {
     depthFirstSearchByStack,
     depthFirstSearchRecursive,
     breathFirstSearchByQueue,
+    includesBFS,
+    includesDFSRecursive,
+    sumDFSStack,
+    sumDFSRecursive,
+    sumBFSQueue,
+    minValueDFS,
+    minValueBFS,
+    minValueDFSRecursive,
+    maxSumPathDFS,
   } = require('./binaryTree');
   
   class BST {
@@ -40,7 +49,6 @@ const {
   
   test('depthFirstSearchByStack(root)', () => {
     const tree = new BinaryTree(1).insert([2,3,4,5]);
-    console.log(tree)
     const result = depthFirstSearchByStack(tree);
     expect(result).toEqual([1,2,4,5,3]);
   });
@@ -50,4 +58,52 @@ const {
     const result = depthFirstSearchRecursive(tree);
     expect(result).toEqual(['A', 'B', 'D', 'E', 'C']);
   });
+
+  test('breathFirstSearchByQueue(root)', () => {
+    const tree = new BinaryTree('A').insert(['B', 'C', 'D', 'E']);
+    const result = breathFirstSearchByQueue(tree);
+    expect(result).toEqual(['A', 'B', 'C', 'D', 'E']);
+  });
+
+  test('includesBFS(root, val) | includesDFSRecursive', () => {
+    const tree1 = new BinaryTree('A').insert(['B', 'C', 'D', 'E']);
+    const tree2 = new BinaryTree(1).insert([2,3,4,5])
+    const bfsResult1 = includesBFS(tree1, 'E');
+    const bfsResult2 = includesBFS(tree2, 8);
+    const dfsResult1 = includesDFSRecursive(tree1, 'E');
+    const dfsResult2 = includesDFSRecursive(tree2, 8);
+    expect(bfsResult1).toEqual(true);
+    expect(bfsResult2).toEqual(false);
+    expect(dfsResult1).toEqual(true);
+    expect(dfsResult2).toEqual(false);
+  });
+
+  test('sumDFSStack | sumDFSRecursive | sumBFSQueue', () => {
+    const tree = new BinaryTree(3).insert([11, 4, 4, 2, 1])
+    const dfsStack = sumDFSStack(tree);
+    const dfsRecursive = sumDFSRecursive(tree);
+    const bfsQueue = sumBFSQueue(tree);
+    expect(dfsStack).toEqual(25);
+    expect(dfsRecursive).toEqual(25);
+    expect(bfsQueue).toEqual(25);
+  });
   
+  test('minValueDFS | minValueBFS | minValueDFSRecursive', () => {
+    const tree = new BinaryTree(3).insert([11, 4, 4, 2, 1])
+    const dfsStack = minValueDFS(tree);
+    const dfsRecursive = minValueDFSRecursive(tree);
+    const bfsQueue = minValueBFS(tree);
+    expect(dfsStack).toEqual(1);
+    expect(dfsRecursive).toEqual(1);
+    expect(bfsQueue).toEqual(1);
+  });
+
+  test('maxSumPathDFS |  | ', () => {
+    const tree = new BinaryTree(3).insert([11, 4, 4, 2, 1])
+    const dfsStack = maxSumPathDFS(tree);
+    // const dfsRecursive = minValueDFSRecursive(tree);
+    // const bfsQueue = minValueBFS(tree);
+    expect(dfsStack).toEqual(18);
+    // expect(dfsRecursive).toEqual(1);
+    // expect(bfsQueue).toEqual(1);
+  });
